cabc5ec4101e7a9ed64d4f68de7c48a8d03a8341
zzoaJW <i7can7i@gmail.com>
2023-01-10 02:38:36 +0900

    Function implementation: When button is pressed, Get current position + Create marker dynamic in current position + Move camera to current position

--- app/src/main/java/com/z0o0a/maptest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/maptest/MainActivity.kt
@@ -1,19 +1,108 @@
 package com.z0o0a.maptest
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.z0o0a.maptest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private var myGoogleMap : GoogleMap? = null
+    private var myCoo = LatLng(0.0, 0.0)
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        // 구글 맵
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // 현재 위치
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+
+        binding.btnCurrentPosi.setOnClickListener {
+            // 현재 위치(위도,경도)에 마커 생성 + 카메라 이동
+
+            // 현재 위치(위도,경도) 토스트 메시지
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        setMyCoo(location.latitude, location.longitude)
+
+                        // 현재 위치 마커 생성
+                        myGoogleMap!!.addMarker(
+                            MarkerOptions()
+                                .position(myCoo)
+                                .title("내 위치")
+                        )
+                        // 현재 위치로 카메라 이동
+                        myGoogleMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(myCoo, 17f))
+
+                    }
+                }
+            }else if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+            }else if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 200)
+            }else{
+                Toast.makeText(this, "현재 위치 불러오기 실패", Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
+
+    // 지도 초기 설정
+    override fun onMapReady(googleMap: GoogleMap) {
+        myGoogleMap = googleMap
+
+        // 좌표
+        val seoul = LatLng(37.5666805, 126.9784147)
+        // 지도 모양 (인공위성, 노말 등)
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        // 지도 위 마커
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(seoul) // 좌표 설정
+                .title("서울 시청") // 마커 이름 설정
+                .snippet("대한민국 서울의 시청이다.") // 제목 아래에 표시되는 설명
+                .alpha(0.7f) // 마커 투명도
+        )
+        // 특정 좌표로 카메라 이동
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+        // 카메라 줌 (위 코드와 중첩시 이 코드를 우선)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 17f))
+    }
+
+    private fun setMyCoo(lat: Double, lon: Double){
+        myCoo = LatLng(lat, lon)
+    }
+
 }
